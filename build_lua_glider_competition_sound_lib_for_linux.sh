3e973cfee040af96bf341d5bbcb360f8d5530dee
witon <witon@qq.com>
2021-05-26 00:35:36 +0800

    solve lua_glider_competition_sound_lib dependency

--- build_lua_glider_competition_sound_lib_for_linux.sh
+++ build_lua_glider_competition_sound_lib_for_linux.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/witon/lua_glider_competition_sound_lib.git
 cd lua_glider_competition_sound_lib
 cmake . -B build
-cp build/sound.so .
+cd build
+cmake --build .
+cp sound.so ..
