5967243129155cdd07f2533772dfb7a45e379fc5
witon <witon@qq.com>
2021-12-14 08:14:29 +0800

    fix adjust.lua crash

--- CompileFiles.lua
+++ CompileFiles.lua
@@ -1,115 +1,227 @@
-local files = {
-	"emutest/luaForTestLoadModule.lua",
-	"emutest/testCfg.lua",
-	"emutest/testCfgO.lua",
-	"emutest/testDataFileDecode.lua",
-	"emutest/testLoadModule.lua",
-	"emutest/testManagerOutput.lua",
-	"emutest/testOutputCurveManager.lua",
-	"emutest/testSinkRateRecord.lua",
-	"LAOZHU/Cfg.lua",
-	"LAOZHU/CfgO.lua",
-	"LAOZHU/comm/LinuxSound.lua",
-	"LAOZHU/comm/OTSound.lua",
-	"LAOZHU/comm/PCIO.lua",
-	"LAOZHU/comm/TestSound.lua",
-	"LAOZHU/comm/Timer.lua",
-	"LAOZHU/DataFileDecode.lua",
-	"LAOZHU/EmuTestUtils.lua",
-	"LAOZHU/F3k/F3kFlightRecord.lua",
-	"LAOZHU/F3k/f3kReadVarMap.lua",
-	"LAOZHU/F3k/F3kState.lua",
-	"LAOZHU/F3kWF/AULDWF.lua",
-	"LAOZHU/F3kWF/CommonTaskWF.lua",
-	"LAOZHU/F3kWF/F3kCompetitionWF.lua",
-	"LAOZHU/F3kWF/F3kRoundWF.lua",
-	"LAOZHU/f5jReadVarMap.lua",
-	"LAOZHU/F5jState.lua",
-	"LAOZHU/LuaUtils.lua",
-	"LAOZHU/Monitor.lua",
-	"LAOZHU/OTUtils.lua",
-	"LAOZHU/Queue.lua",
-	"LAOZHU/readVar.lua",
-	"LAOZHU/Sensor.lua",
-	"LAOZHU/SinkRateLog.lua",
-	"LAOZHU/SinkRateRecord.lua",
-	"LAOZHU/SinkRateState.lua",
-	"LAOZHU/SwitchTrigeDetector.lua",
-	"TELEMETRY/3k/f3kCore.lua",
-	"TELEMETRY/3k/F3KRecordListView.lua",
-	"TELEMETRY/3k/FlightPage.lua",
-	"TELEMETRY/3k/FlightPageNew.lua",
-	"TELEMETRY/3k/FlightStaticPage.lua",
-	"TELEMETRY/3k/RoundSetupPage.lua",
-	"TELEMETRY/3k/SetupPage.lua",
-	"TELEMETRY/3k/SmallFontFlightListPage.lua",
-	"TELEMETRY/3k/TaskSelector.lua",
-	"TELEMETRY/3k/TaskSelectorO.lua",
-	"TELEMETRY/3ktel.lua",
-	"TELEMETRY/5j/FlightPage.lua",
-	"TELEMETRY/5j/LargeFontFlightListPage.lua",
-	"TELEMETRY/5j/SetupPage.lua",
-	"TELEMETRY/5j/SmallFontFlightListPage.lua",
-	"TELEMETRY/5jtel.lua",
-	"TELEMETRY/adjust/BackupOutput.lua",
-	"TELEMETRY/adjust/GlobalVar.lua",
-	"TELEMETRY/adjust/ManagerOutput.lua",
-	"TELEMETRY/adjust/output.lua",
-	"TELEMETRY/adjust/OutputCurve.lua",
-	"TELEMETRY/adjust/OutputCurveManager.lua",
-	"TELEMETRY/adjust/Output_old.lua",
-	"TELEMETRY/adjust/ReplaceMix.lua",
-	"TELEMETRY/adjust/SaveAndRestoreParam.lua",
-	"TELEMETRY/adjust/SelectChannel.lua",
-	"TELEMETRY/adjust/SinkRate/RecordListView.lua",
-	"TELEMETRY/adjust/SinkRate/SinkRate.lua",
-	"TELEMETRY/adjust/SinkRate/SinkRateCfgPage.lua",
-	"TELEMETRY/adjust.lua",
-	"TELEMETRY/common/Button.lua",
-	"TELEMETRY/common/ButtonO.lua",
-	"TELEMETRY/common/CheckBox.lua",
-	"TELEMETRY/common/CheckBoxO.lua",
-	"TELEMETRY/common/comp.lua",
-	"TELEMETRY/common/CurveSelector.lua",
-	"TELEMETRY/common/CurveSelectorO copy.lua",
-	"TELEMETRY/common/CurveSelectorO.lua",
-	"TELEMETRY/common/Fields.lua",
-	"TELEMETRY/common/InputSelector.lua",
-	"TELEMETRY/common/InputSelectorO.lua",
-	"TELEMETRY/common/InputView.lua",
-	"TELEMETRY/common/InputViewO.lua",
-	"TELEMETRY/common/LoadModule.lua",
-	"TELEMETRY/common/ModeSelector.lua",
-	"TELEMETRY/common/ModeSelectorO.lua",
-	"TELEMETRY/common/NumEdit.lua",
-	"TELEMETRY/common/NumEditO.lua",
-	"TELEMETRY/common/OutputSelector.lua",
-	"TELEMETRY/common/OutputSelectorO.lua",
-	"TELEMETRY/common/Selector.lua",
-	"TELEMETRY/common/SelectorO.lua",
-	"TELEMETRY/common/TextEdit.lua",
-	"TELEMETRY/common/TextEditO.lua",
-	"TELEMETRY/common/TimeEdit.lua",
-	"TELEMETRY/common/TimeEditO.lua",
-	"TELEMETRY/common/ViewMatrix.lua",
-	"TELEMETRY/common/ViewMatrixO.lua",
-	"TELEMETRY/ut.lua",
-	"TELEMETRY/utO.lua",
-	"test/test.lua",
-	"test/TestF3kCompetitionWF.lua",
-	"test/TestF3kFlightRecord.lua",
-	"test/TestF3kRound.lua",
-	"test/TestF3kState.lua",
-	"test/testF3kTask/TestAULD.lua",
-	"test/testF3kTask/TestCommonTask.lua",
-	"test/TestF5jState.lua",
-	"test/TestLaozhuUtils.lua",
-	"test/TestQueue.lua",
-	"test/TestReadVar.lua",
-	"test/TestSensor.lua",
-	"test/TestSinkRateState.lua",
-	"test/TestSwitchTrigeDetector.lua",
-	"test/TestTimer.lua",
-	"test/utils4Test.lua",
-}
+local files = {
+
+	"emutest/luaForTestLoadModule.lua",
+
+	"emutest/testCfg.lua",
+
+	"emutest/testCfgO.lua",
+
+	"emutest/testDataFileDecode.lua",
+
+	"emutest/testLoadModule.lua",
+
+	"emutest/testManagerOutput.lua",
+
+	"emutest/testOutputCurveManager.lua",
+
+	"emutest/testSinkRateRecord.lua",
+
+	"LAOZHU/Cfg.lua",
+
+	"LAOZHU/CfgO.lua",
+
+	"LAOZHU/comm/LinuxSound.lua",
+
+	"LAOZHU/comm/OTSound.lua",
+
+	"LAOZHU/comm/PCIO.lua",
+
+	"LAOZHU/comm/TestSound.lua",
+
+	"LAOZHU/comm/Timer.lua",
+
+	"LAOZHU/DataFileDecode.lua",
+
+	"LAOZHU/EmuTestUtils.lua",
+
+	"LAOZHU/F3k/F3kFlightRecord.lua",
+
+	"LAOZHU/F3k/f3kReadVarMap.lua",
+
+	"LAOZHU/F3k/F3kState.lua",
+
+	"LAOZHU/F3kWF/AULDWF.lua",
+
+	"LAOZHU/F3kWF/CommonTaskWF.lua",
+
+	"LAOZHU/F3kWF/F3kCompetitionWF.lua",
+
+	"LAOZHU/F3kWF/F3kRoundWF.lua",
+
+	"LAOZHU/f5jReadVarMap.lua",
+
+	"LAOZHU/F5jState.lua",
+
+	"LAOZHU/LuaUtils.lua",
+
+	"LAOZHU/Monitor.lua",
+
+	"LAOZHU/OTUtils.lua",
+
+	"LAOZHU/Queue.lua",
+
+	"LAOZHU/readVar.lua",
+
+	"LAOZHU/Sensor.lua",
+
+	"LAOZHU/SinkRateLog.lua",
+
+	"LAOZHU/SinkRateRecord.lua",
+
+	"LAOZHU/SinkRateState.lua",
+
+	"LAOZHU/SwitchTrigeDetector.lua",
+
+	"TELEMETRY/3k/f3kCore.lua",
+
+	"TELEMETRY/3k/F3KRecordListView.lua",
+
+	"TELEMETRY/3k/FlightPage.lua",
+
+	"TELEMETRY/3k/FlightPageNew.lua",
+
+	"TELEMETRY/3k/FlightStaticPage.lua",
+
+	"TELEMETRY/3k/RoundSetupPage.lua",
+
+	"TELEMETRY/3k/SetupPage.lua",
+
+	"TELEMETRY/3k/SmallFontFlightListPage.lua",
+
+	"TELEMETRY/3k/TaskSelector.lua",
+
+	"TELEMETRY/3k/TaskSelectorO.lua",
+
+	"TELEMETRY/3ktel.lua",
+
+	"TELEMETRY/5j/FlightPage.lua",
+
+	"TELEMETRY/5j/LargeFontFlightListPage.lua",
+
+	"TELEMETRY/5j/SetupPage.lua",
+
+	"TELEMETRY/5j/SmallFontFlightListPage.lua",
+
+	"TELEMETRY/5jtel.lua",
+
+	"TELEMETRY/adjust/BackupOutput.lua",
+
+	"TELEMETRY/adjust/GlobalVar.lua",
+
+	"TELEMETRY/adjust/ManagerOutput.lua",
+
+	"TELEMETRY/adjust/output.lua",
+
+	"TELEMETRY/adjust/OutputCurve.lua",
+
+	"TELEMETRY/adjust/OutputCurveManager.lua",
+
+	"TELEMETRY/adjust/Output_old.lua",
+
+	"TELEMETRY/adjust/ReplaceMix.lua",
+
+	"TELEMETRY/adjust/SaveAndRestoreParam.lua",
+
+	"TELEMETRY/adjust/SelectChannel.lua",
+
+	"TELEMETRY/adjust/SinkRate/RecordListView.lua",
+
+	"TELEMETRY/adjust/SinkRate/SinkRate.lua",
+
+	"TELEMETRY/adjust/SinkRate/SinkRateCfgPage.lua",
+
+	"TELEMETRY/adjust.lua",
+
+	"TELEMETRY/common/Button.lua",
+
+	"TELEMETRY/common/ButtonO.lua",
+
+	"TELEMETRY/common/CheckBox.lua",
+
+	"TELEMETRY/common/CheckBoxO.lua",
+
+	"TELEMETRY/common/comp.lua",
+
+	"TELEMETRY/common/CurveSelector.lua",
+
+	"TELEMETRY/common/CurveSelectorO.lua",
+
+	"TELEMETRY/common/Fields.lua",
+
+	"TELEMETRY/common/InputSelector.lua",
+
+	"TELEMETRY/common/InputSelectorO.lua",
+
+	"TELEMETRY/common/InputView.lua",
+
+	"TELEMETRY/common/InputViewO.lua",
+
+	"TELEMETRY/common/LoadModule.lua",
+
+	"TELEMETRY/common/ModeSelector.lua",
+
+	"TELEMETRY/common/ModeSelectorO.lua",
+
+	"TELEMETRY/common/NumEdit.lua",
+
+	"TELEMETRY/common/NumEditO.lua",
+
+	"TELEMETRY/common/OutputSelector.lua",
+
+	"TELEMETRY/common/OutputSelectorO.lua",
+
+	"TELEMETRY/common/Selector.lua",
+
+	"TELEMETRY/common/SelectorO.lua",
+
+	"TELEMETRY/common/TextEdit.lua",
+
+	"TELEMETRY/common/TextEditO.lua",
+
+	"TELEMETRY/common/TimeEdit.lua",
+
+	"TELEMETRY/common/TimeEditO.lua",
+
+	"TELEMETRY/common/ViewMatrix.lua",
+
+	"TELEMETRY/common/ViewMatrixO.lua",
+
+	"TELEMETRY/ut.lua",
+
+	"TELEMETRY/utO.lua",
+
+	"test/test.lua",
+
+	"test/TestF3kCompetitionWF.lua",
+
+	"test/TestF3kFlightRecord.lua",
+
+	"test/TestF3kRound.lua",
+
+	"test/TestF3kState.lua",
+
+	"test/testF3kTask/TestAULD.lua",
+
+	"test/testF3kTask/TestCommonTask.lua",
+
+	"test/TestF5jState.lua",
+
+	"test/TestLaozhuUtils.lua",
+
+	"test/TestQueue.lua",
+
+	"test/TestReadVar.lua",
+
+	"test/TestSensor.lua",
+
+	"test/TestSinkRateState.lua",
+
+	"test/TestSwitchTrigeDetector.lua",
+
+	"test/TestTimer.lua",
+
+	"test/utils4Test.lua",
+
+}
+
 return files
