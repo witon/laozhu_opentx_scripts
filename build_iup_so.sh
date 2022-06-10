c0ac4414a76b0bdf238b73540706a9aadba9fa1c
witon <witon@qq.com>
2022-06-10 19:03:48 +0800

    Update build_iup_so.sh

--- build_iup_so.sh
+++ build_iup_so.sh
@@ -8,16 +8,17 @@ make
 cd ..
 cd lua53
 make linux
+cp src/liblua.a src/liblua53.a
 export LUA_LIB=`pwd`/src
 cd ..
 cd im
 export USE_LUA53=Yes
-./config_lua_module
+bash config_lua_module
 make
 cd ..
 cd cd
 make
-./install_dev
+bash install_dev
 cd ..
 cd iup
 make
