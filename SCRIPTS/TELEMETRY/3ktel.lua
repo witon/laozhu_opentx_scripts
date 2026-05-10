1cae26a7077cb4deadc36082a9ce995f25c5bfaf
witon <witon@qq.com>
2026-05-10 14:38:55 +0800

    refactor: restructure f3kFramework for improved modularity and state management
    - Consolidated state management within `f3kFramework.lua`, removing global state variables and encapsulating them in a local structure.
    - Refactored navigation and page handling functions to enhance readability and maintainability, including the introduction of local functions for loading and unloading pages.
    - Updated the telemetry and widget scripts to utilize the new framework structure, streamlining initialization and event handling processes.
    
    These changes enhance the organization and efficiency of the framework, improving overall code clarity and modularity.

--- SCRIPTS/TELEMETRY/3ktel.lua
+++ SCRIPTS/TELEMETRY/3ktel.lua
@@ -6,24 +6,11 @@ gF3kCore = nil
 
 f3kCfg = nil
 
-local Nav = LZ_runModule(gSDCardDir .. "LAOZHU/3k/f3kFramework.lua")
-local state = {
-	pages = Nav.FLIGHT_PAGE_PATHS,
-	displayIndex = 1,
-	curPage = nil,
-	lastEvent = 0,
-}
-
-
-LZ_runModule(gSDCardDir .. "LAOZHU/uilib/keyMap.lua")
-local keyMap = KMgetKeyMap();
-KMunload();
-
+local f3kFramework = LZ_runModule(gSDCardDir .. "LAOZHU/3k/f3kFramework.lua")
 
 local function init()
-	Nav.telInit(state)
+	f3kFramework.initFramework()
 end
-
 
 local function background()
 	if gF3kCore == nil then
@@ -33,41 +20,8 @@ local function background()
 end
 
 local function run(event)
-
-	e = keyMap[event];
-	if false then
-		if event ~= 0 then
-			lcd.clear()
-			lcd.drawText(10, 10, "event:", LZ_ui.font)
-			lcd.drawNumber(10, 20, event, LZ_ui.font)
-			lcd.drawText(10, 30, "e:", LZ_ui.font)
-			if e ~= nil then
-				lcd.drawNumber(10, 40, e, LZ_ui.font)
-			end
-		end
-		return
-	else
-		lcd.clear()
-	end
-
-	if e ~= nil then
-		event = e;
-	end
-
-	local curTime = getTime()
-	if state.curPage == nil then
-		Nav.loadPage(state)
-	end
-	
-	
-	local eventProcessed = state.curPage.run(event, curTime)
-	if eventProcessed then
-		return
-	end
-	Nav.handleNavAfterPage(state, event)
-	return true;
-
-
+	lcd.clear()
+	return f3kFramework.run(event)
 end
 
 return { run=run, init=init, background=background }
